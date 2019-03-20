4e91a492feb91aa957fa40d515d64bd0d12486ff
AaronC99 <aaronchuah99@gmail.com>
2019-03-20 13:57:50 +0800

    Updated checkIn.py

--- employee_CheckIn.py
+++ employee_CheckIn.py
@@ -46,7 +46,7 @@ while continue_reading:
     # If we have the UID, continue
     if status == MIFAREReader.MI_OK:
 
-        rfid_access.execute("INSERT OR IGNORE INTO attendance(employeeID,clockIn,lateness) VALUES ((SELECT employee_uid FROM employee where card_uid = ?) , strftime('%H:%M','now','localtime'),(strftime('%H','now','localtime') - strftime('%H','09:00'))|| ':' || (strftime('%M','now','localtime') - strftime('%M','09:00')) )",[card_uid])
+        rfid_access.execute("INSERT OR IGNORE INTO attendance(employeeID,clockIn,lateness,date) VALUES ((SELECT employee_uid FROM employee where card_uid = ?) , strftime('%H:%M','now','localtime'),(strftime('%H','now','localtime') - strftime('%H','09:00'))|| ':' || (strftime('%M','now','localtime') - strftime('%M','09:00')), date('now','localtime') )",[card_uid])
         rfidData.commit() # connection for COMMIT
     
     time.sleep(1)
